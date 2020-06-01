9f3452613c09fab68cbfbf32e60c2ed8d79e9c95
Matheus Verissimo <mverissimoo@hotmail.com>
2020-06-01 10:34:02 -0300

    feat(utils): add media breakpoint validator

--- src/utils/media.ts
+++ src/utils/media.ts
@@ -4,5 +4,9 @@ import config from '../config';
 export function media(breakpoint: Breakpoints) {
   const breakpoints = config().breakpoints;
 
+  if (!Object.keys(breakpoints).includes(breakpoint)) {
+    throw new Error(`Breakpoint '${breakpoint}' not found`);
+  }
+
   return `@media (min-width: ${breakpoints[breakpoint]}em)`;
 }
