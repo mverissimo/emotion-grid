f5f2d57fb33c04aadb131dd4d2a89f83e9e2b090
Matheus Verissimo <mverissimoo@hotmail.com>
2020-06-12 15:50:50 -0300

    chore: export components

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export { config } from './config';
 export { Container } from './components/Container';
 export { Row } from './components/Row';
 export { Col } from './components/Col';
+export { Hidden } from './components/Hidden';
+export { Visible } from './components/Visible';
+export { ScreenClass } from './components/ScreenClass';
+export { Debugger } from './components/Debugger';
