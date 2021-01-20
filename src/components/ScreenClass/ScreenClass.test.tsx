3f8d7500b1444ead45789d1cf357e1a19e32909f
Matheus Verissimo <mverissimoo@hotmail.com>
2021-01-20 18:03:44 -0300

    test(components): update tests

--- src/components/ScreenClass/ScreenClass.test.tsx
+++ src/components/ScreenClass/ScreenClass.test.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import mediaQuery from 'css-mediaquery';
 import { render } from '@testing-library/react';
 
-import { config } from '../../config';
 import { ScreenClass } from '.';
 
-const createMatchMedia = (width: number) => (query: string) => ({
-  matches: mediaQuery.match(query, { width }),
-});
-
 describe('ScreenClass', () => {
-  it.each(Object.values(config().breakpoints))(
-    'should render with current screen %s',
-    (breakpoint) => {
-      // @ts-ignore
-      window.matchMedia = createMatchMedia(breakpoint * 16);
+  it('should render with current screen %s', () => {
+    const { container } = render(
+      <ScreenClass
+        render={(screen) => {
+          return <div>{screen}</div>;
+        }}
+      />
+    );
 
-      const { container } = render(
-        <ScreenClass
-          render={(screen) => {
-            return <div>{screen}</div>;
-          }}
-        />
-      );
-
-      expect(container.firstChild).toMatchSnapshot();
-    }
-  );
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
