71b198f20afb2269540b4bf0eace2d023791cc4d
Matheus Verissimo <mverissimoo@hotmail.com>
2021-04-02 19:16:00 -0300

    chore(src): export types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 export * from './utils';
 export { config } from './config';
+
+/**
+ * Components
+ */
 export { Container } from './components/Container';
+export type { ContainerProps } from './components/Container';
+
 export { Row } from './components/Row';
+export type { RowProps } from './components/Row';
+
 export { Col } from './components/Col';
+export type { ColProps } from './components/Col';
+
+/**
+ * Utilities
+ */
 export { Hidden } from './components/Hidden';
+export type { HiddenProps } from './components/Hidden';
+
 export { Visible } from './components/Visible';
+export type { VisibleProps } from './components/Visible';
+
 export { ScreenClass } from './components/ScreenClass';
+export type { ScreenClassProps } from './components/ScreenClass';
+
 export { Debugger } from './components/Debugger';
