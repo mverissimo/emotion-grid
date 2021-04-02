cb6606edc0dbc8103ad5d547fadf71468004817a
Matheus Verissimo <mverissimoo@hotmail.com>
2021-04-02 18:38:56 -0300

    refactor(theme): update types

---
+++ src/types/emotion.d.ts
@@ -0,0 +1,56 @@
+import '@emotion/react';
+
+export type Breakpoints = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+/**
+ * Flexbox properties
+ */
+export type Align =
+  | 'baseline'
+  | 'center'
+  | 'flex-start'
+  | 'flex-end'
+  | 'stretch';
+
+export type Justify =
+  | 'center'
+  | 'flex-end'
+  | 'flex-start'
+  | 'space-around'
+  | 'space-between';
+
+export interface AlignProps {
+  xs?: Align;
+  sm?: Align;
+  md?: Align;
+  lg?: Align;
+  xl?: Align;
+}
+
+export interface JustifyProps {
+  xs?: Justify;
+  sm?: Justify;
+  md?: Justify;
+  lg?: Justify;
+  xl?: Justify;
+}
+
+export interface ThemeBase {
+  grid?: {
+    container: Record<Breakpoints, number | string>;
+    padding: Record<Breakpoints, number>;
+    columns: Record<Breakpoints, number>;
+    gutter: Record<Breakpoints, number>;
+    breakpoints: Record<Breakpoints, number>;
+    colors: {
+      blue: string;
+    };
+  };
+}
+export interface StyleProps {
+  theme: ThemeBase;
+}
+
+declare module '@emotion/react' {
+  export interface Theme extends ThemeBase {}
+}
